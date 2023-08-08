ed9069d53920e87ab0034737edef0c1408af8822
Caishangqi <2470159730@qq.com>
2023-08-08 15:01:43 +0800

    23.08.08.02 Change Project version

--- Source/CharmingCraftEditor.Target.cs
+++ Source/CharmingCraftEditor.Target.cs
@@ -9,7 +9,7 @@ public class CharmingCraftEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
 		ExtraModuleNames.Add("CharmingCraft");
 	}
 }
