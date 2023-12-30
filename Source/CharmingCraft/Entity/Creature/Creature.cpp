117e776dc3fa2326a48c54a7eb8c38647060c588
Caishangqi <2470159730@qq.com>
2023-12-30 16:55:31 +0800

    Add Skeleton Enemy and Patrolling state - Press A to change AI state Patrolling/Attacking

---
+++ Source/CharmingCraft/Entity/Creature/Creature.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Creature.h"
+
+
+// Sets default values
+ACreature::ACreature()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACreature::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACreature::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ACreature::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/CharmingCraft/Entity/Creature/Creature.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "CharmingCraft/Interface/ActionOnHitInterface.h"
+#include "CharmingCraft/Interface/DGameplayInterface.h"
+#include "GameFramework/Character.h"
+#include "Creature.generated.h"
+
+UCLASS()
+class CHARMINGCRAFT_API ACreature : public ACharacter, public IActionOnHitInterface, public IDGameplayInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACreature();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="AI")
+	TObjectPtr<UBehaviorTree> BehaviourTree;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
